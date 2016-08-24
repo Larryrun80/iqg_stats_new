a834730633ca44bcb0a429761e8dd060b6e512bf
Larry Guo <larryrun80@gmail.com>
2016-08-24 18:07:27 +0800

    fix log time

--- stats/scripts/script_log.py
+++ stats/scripts/script_log.py
@@ -3,7 +3,7 @@ import arrow
 
 def print_log(message, m_type='INFO'):
     m_types = ('INFO', 'WARNING', 'ERROR')
-    prefix = '[ {} ]'.format(arrow.now().format('YYYY-MM-DD-SSS'))
+    prefix = '[ {} ]'.format(arrow.now().format('YYYY-MM-DD HH:mm:ss:SSS'))
     if str(m_type).upper() in m_types:
         m_type = str(m_type).upper()
     else:
