c91b35d21f482fd9b7aa1e7827f8ee775b645719
Larry Guo <larryrun80@gmail.com>
2016-11-11 10:06:20 +0800

    remove sync merchant_ext and add sku_promotion

--- stats/scripts/hsq_data_sync.py
+++ stats/scripts/hsq_data_sync.py
@@ -7,7 +7,7 @@ import sys
 
 CONFIG_HSQ_SECTION = 'HSQ_MYSQL'
 CONFIG_STATS_SECTION = 'STATS_MYSQL'
-SYNC_TABLES = ('user', 'trade_order', 'merchant', 'merchant_ext')
+SYNC_TABLES = ('user', 'trade_order', 'merchant', 'sku_promotion')
 
 
 def get_columns(cnx, table_name):
