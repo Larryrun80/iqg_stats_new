5ca680f612de75c259ef6de2bfd3c119435d7e8b
Larry Guo <larryrun80@gmail.com>
2017-05-08 16:21:02 +0800

    update lottery method and fix wx template bugs

--- stats/scripts/hsq_wx_template.py
+++ stats/scripts/hsq_wx_template.py
@@ -16,7 +16,7 @@ WRITE_PATH = '{root_dir}/{file_dir}/{file_name}'.format(
             )
 EXCLUDE = ('brief', 'template')
 WS_KEYS = ('类型', '开始时间', '结束时间', '说明', 'sql', '条件')
-TYPES = ('拼团', '抽奖', '其他')
+TYPES = ('拼团', '抽奖', '领券', '其他')
 
 
 def get_sheets(wb):
@@ -24,7 +24,7 @@ def get_sheets(wb):
     return wb.get_sheet_names()
 
 
-def check_format(ws):
+def get_info(ws, cnx):
     query_data = {}
     for i in range(len(WS_KEYS)):
         if WS_KEYS[i] != ws['A{}'.format(i+1)].value:
@@ -35,7 +35,7 @@ def check_format(ws):
             if i < 5:
                 query_data[WS_KEYS[i]] = ws['B{}'.format(i+1)].value
             else:
-                query_data['条件'] = get_conditions(ws, query_data['类型'])
+                query_data['条件'] = get_conditions(ws, query_data['类型'], cnx)
 
     if query_data['类型'] not in TYPES:
         raise RuntimeError(
@@ -58,9 +58,9 @@ def check_format(ws):
     return query_data
 
 
-def get_conditions(ws, qtype):
+def get_conditions(ws, qtype, cnx):
     vals = []
-    if qtype in ('拼团', '抽奖'):
+    if qtype in ('拼团', '抽奖', '领券'):
         for i in range(5, ws.max_row):
             if ws.cell(row=i+1, column=2).value:
                 try:
@@ -68,6 +68,24 @@ def get_conditions(ws, qtype):
                 except:
                     raise RuntimeError(
                         'format error! not valid condition found')
+
+    if qtype == '领券':
+        coupon_ids = []
+        sql = '''
+                select distinct(coupon_id)
+                from reward_rules
+                where reward_id in ({aids})
+        '''.format(aids=','.join(vals))
+
+        cursor = cnx.cursor()
+        cursor.execute(sql)
+        coupons = cursor.fetchall()
+        cursor.close()
+
+        for (coupon_id,) in coupons:
+            coupon_ids.append(str(coupon_id))
+
+        vals = coupon_ids
 
     return vals
 
@@ -77,7 +95,8 @@ def generate_sql(qd, exclude_users):
 
     if qd['类型'] == '拼团':
         sql = '''
-                select distinct(o.user_id), u.wechat_mp_open_id
+                select distinct(o.user_id), u.wechat_mp_open_id,
+                       o.pin_event_status status
                 from pin_activities_order o
                 inner join user_login_info u on u.user_id=o.user_id
                 where pin_activities_id in ({pids})
@@ -91,9 +110,26 @@ def generate_sql(qd, exclude_users):
                 from pin_activities_order o
                 inner join user_login_info u on u.user_id=o.user_id
                 where pin_activities_id in ({pids})
+                and o.user_id not in (
+                select user_id from (
+                    select user_id, count(0) cnt, sum(is_winner) win
+                    from pin_activities_order o
+                    inner join pin_activities a on o.pin_activities_id=a.id
+                    where a.type=2
+                    group by user_id)t where cnt>9 and win=0
+                )
                 and (u.wechat_mp_open_id is not null
                      and u.wechat_mp_open_id != '')
         '''.format(pids=','.join(qd['条件']))
+    elif qd['类型'] == '领券':
+        sql = '''
+                select distinct(o.user_id), ul.wechat_mp_open_id
+                from trade_order_coupon o
+                inner join `user_login_info` ul on ul.user_id = o.user_id
+                where o.coupon_id in ({cids})
+                and (ul.wechat_mp_open_id is not null
+                     and ul.wechat_mp_open_id != '')
+        '''.format(cids=','.join(qd['条件']))
     elif qd['类型'] == '其他':
         sql = qd['sql']
 
@@ -119,13 +155,29 @@ def get_users(cnx, sqlstr):
 
 
 def write_results(worksheet, qd_type, users, selected_users):
-    if qd_type in ('拼团', '其他'):
+    if qd_type in ('领券', '其他'):
         worksheet['A1'] = 'User_ID'
         worksheet['B1'] = 'Open_ID'
         for i, user in enumerate(users, 2):
             selected_users.append(str(user[0]))
             worksheet['A{}'.format(i)] = user[0]
             worksheet['B{}'.format(i)] = user[1]
+    if qd_type == '拼团':
+        worksheet['A1'] = 'User_ID [拼团失败]'
+        worksheet['B1'] = 'Open_ID [拼团失败]'
+        worksheet['D1'] = 'User_ID [拼团成功]'
+        worksheet['E1'] = 'Open_ID [拼团成功]'
+        row_index = [2, 2]
+        for user in users:
+            selected_users.append(str(user[0]))
+            if user[-1] == 3:
+                worksheet['A{}'.format(row_index[0])] = user[0]
+                worksheet['B{}'.format(row_index[0])] = user[1]
+                row_index[0] += 1
+            if user[-1] == 2:
+                worksheet['D{}'.format(row_index[1])] = user[0]
+                worksheet['E{}'.format(row_index[1])] = user[1]
+                row_index[1] += 1
     if qd_type == '抽奖':
         worksheet['A1'] = 'User_ID [拼团失败]'
         worksheet['B1'] = 'Open_ID [拼团失败]'
@@ -168,7 +220,7 @@ if __name__ == '__main__':
                 print('working on worksheet {} ...'.format(ws_name))
                 ws = wb.get_sheet_by_name(ws_name)
                 print('    checking format ...')
-                qd = check_format(ws)
+                qd = get_info(ws, cnx)
                 qd['sql'] = generate_sql(qd, selected_users)
 
                 print('    getting user info ...')
@@ -179,6 +231,7 @@ if __name__ == '__main__':
                 write_results(result_ws, qd['类型'], users, selected_users)
 
         result_wb.save(WRITE_PATH)
+        print('select {} users totally'.format(len(selected_users)))
         print('done!')
     except Exception as e:
         print(e)
